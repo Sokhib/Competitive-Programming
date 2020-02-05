bf2faa17b230398ad8800b371c2906c470f15a56
Sokhibdzhon <sokhibsaid@gmail.com>
2020-02-05 15:45:52 +0300

    Just replace . with [.] :)

---
+++ src/Leetcode/DefangingIPAddress.kt
@@ -0,0 +1,13 @@
+package Leetcode
+
+class Solution1 {
+    fun defangIPaddr(address: String): String {
+        return address.replace(".", "[.]")
+    }
+}
+
+
+fun main() {
+    val sol1 = Solution1()
+    println(sol1.defangIPaddr("255.100.50.0"))
+}
