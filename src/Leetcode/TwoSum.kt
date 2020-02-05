adbd1ef445bdd85fe672e16c6b292af62d6dc944
Sokhibdzhon <sokhibsaid@gmail.com>
2020-02-05 15:13:37 +0300

    Two Sum solution: using HashMap(memory++, speed--) for every element of array we subtract(-) from target and check our HashMap KEY if it contains this result. If it does not contain result we will add array element as KEY and it's index as VALUE. If HashMap contains result we return HashMap VALUE (index of element) and INDEX of current element we subtracted from target.

--- src/Leetcode/TwoSum.kt
+++ src/Leetcode/TwoSum.kt
@@ -2,12 +2,13 @@ package Leetcode
 
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0 until nums.size - 1) {
-            for (j in i + 1 until nums.size) {
-                if (nums[i] + nums[j] == target) {
-                    return intArrayOf(i, j)
-                }
+        val hashMap = HashMap<Int, Int>()
+
+        nums.forEachIndexed { index, value ->
+            if (hashMap.containsKey(target - value)) {
+                return intArrayOf(hashMap[target - value]!!, index)
             }
+            hashMap[value] = index
         }
         return intArrayOf()
     }
@@ -16,7 +17,7 @@ class Solution {
 fun main() {
 
     val sol = Solution()
-    val res = sol.twoSum(intArrayOf(2, 3, 9, 7), 9)
+    val res = sol.twoSum(intArrayOf(2, 7, 9, 2), 9)
     res.forEach {
         println(it)
     }
