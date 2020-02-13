54b5f3a16b887456a14b0342df931ac5052a25a0
Sokhibdzhon <sokhibsaid@gmail.com>
2020-02-13 19:27:44 +0300

    For each Jewel we will count number of Stones matching...

---
+++ src/Leetcode/JewelsAndStones.kt
@@ -0,0 +1,21 @@
+package Leetcode
+
+class Solution771 {
+
+    fun numJewelsInStones(J: String, S: String): Int {
+        var result = 0
+        J.forEach { jewel ->
+            result += S.count { stone ->
+                stone == jewel
+            }
+        }
+        return result
+    }
+}
+
+fun main() {
+
+    val sol = Solution771()
+    println(sol.numJewelsInStones("aA", "aAAbbbb"))
+    println(sol.numJewelsInStones("z", "ZZ"))
+}
