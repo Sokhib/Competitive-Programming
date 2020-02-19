bd5003e61efd21aafdfd811fac355fd4a4159d91
Sokhibdzhon <sokhibsaid@gmail.com>
2020-02-19 17:23:50 +0300

    Just calculate Product and Sum and subtract it :)

---
+++ src/Leetcode/SubtractTheProductAndSumOfDigitsOfAnInteger.kt
@@ -0,0 +1,22 @@
+package Leetcode
+
+class Solution1281 {
+    fun subtractProductAndSum(n: Int): Int {
+        var givenNumber = n
+        var sum = 0
+        var multiplication = 1
+        while (givenNumber > 0) {
+            sum += (givenNumber % 10)
+            multiplication *= (givenNumber % 10)
+            givenNumber /= 10
+        }
+        return multiplication - sum
+    }
+}
+
+fun main() {
+    val sol = Solution1281()
+
+    println(sol.subtractProductAndSum(234))
+    println(sol.subtractProductAndSum(4421))
+}
