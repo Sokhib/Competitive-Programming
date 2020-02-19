7e01983000a7845d024356afe2e6da334608b4fc
Sokhibdzhon <sokhibsaid@gmail.com>
2020-02-19 17:56:17 +0300

    Just convert integer to String and replace first matched 6 to 9:)

---
+++ src/Leetcode/Maximum69Number.kt
@@ -0,0 +1,25 @@
+package Leetcode
+
+/*
+
+Given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+* */
+
+
+class Solution1323 {
+    fun maximum69Number(num: Int): Int {
+        val strNum = num.toString()
+
+        return strNum.replaceFirst('6', '9').toInt()
+    }
+}
+
+fun main() {
+    val sol = Solution1323()
+    println(sol.maximum69Number(9669))
+    println(sol.maximum69Number(9996))
+    println(sol.maximum69Number(9999))
+}
