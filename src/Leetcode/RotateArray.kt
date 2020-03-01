b2f9420a1649601eb90bc5856cb93ddab46c8c0f
Sokhibdzhon <sokhibsaid@gmail.com>
2020-03-01 20:36:05 +0300

    Just doubled the array and took range from numbers Size-K until DoubledSize-K.

---
+++ src/Leetcode/RotateArray.kt
@@ -0,0 +1,25 @@
+package Leetcode
+
+class Solution189 {
+    fun rotate(nums: IntArray, k: Int) {
+        if (nums.isEmpty() || nums.size == 1 || k < 1) {
+            return
+        }
+        val newFreakingK = k % nums.size
+        val doubledNums = nums + nums
+        val result = doubledNums.sliceArray((nums.size - newFreakingK) until (doubledNums.size - newFreakingK))
+
+        result.forEachIndexed { index, value ->
+            nums[index] = value
+        }
+    }
+}
+
+fun main() {
+
+    val solution189 = Solution189()
+    solution189.rotate(intArrayOf(1, 2, 3, 4, 5, 6, 7), 3)
+//    println()
+//    solution189.rotate(intArrayOf(-1, -100, 3, 99), 2)
+
+}
