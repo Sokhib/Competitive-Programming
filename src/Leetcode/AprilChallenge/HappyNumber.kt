cc7c80e3f1224f5653623975808cb3dad035baaf
Sokhibdzhon <sokhibsaid@gmail.com>
2020-04-07 20:16:35 +0300

    HappyNumber.kt April Challenge solved with straightforward implementation.

---
+++ src/Leetcode/AprilChallenge/HappyNumber.kt
@@ -0,0 +1,29 @@
+package Leetcode.AprilChallenge
+
+object HappyNumber {
+    fun isHappy(n: Int): Boolean {
+        val visited = HashSet<Int>()
+        var number = n
+        while (true) {
+            if (number == 1) return true
+            number = sumOfSquaresOfDigits(number)
+            if (visited.contains(number)) return false
+            visited.add(number)
+        }
+    }
+
+    private fun sumOfSquaresOfDigits(n: Int): Int {
+        var number = n
+        var sum = 0
+        while (number > 0) {
+            val digit = number % 10
+            number /= 10
+            sum += (digit * digit)
+        }
+        return sum
+    }
+}
+
+fun main() {
+    println(HappyNumber.isHappy(19))
+}
