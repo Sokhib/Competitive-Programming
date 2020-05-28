e900e5754985b3fd45b97e0227381722def44fd5
Sokhibdzhon <sokhibsaid@gmail.com>
2020-05-28 05:51:06 +0300

    Solution for SimilarPairs.kt problem of round644div3 contest codeforces.

--- src/codeforces/round644div3/SimilarPairs.kt
+++ src/codeforces/round644div3/SimilarPairs.kt
@@ -1,4 +1,4 @@
-//package codeforces.round644div3
+package codeforces.round644div3
 
 import java.util.*
 
@@ -8,29 +8,21 @@ fun main() {
     repeat(times) {
         val numberOfPairs = reader.nextInt()
         val pairs = Array(numberOfPairs) { 0 }
-        var ones = 0
-        var zeros = 0
         repeat(numberOfPairs) { index ->
             pairs[index] = reader.nextInt()
-            if (pairs[index] % 2 == 0) {
-                ++zeros
-            } else {
-                ++ones
-            }
         }
         pairs.sort()
-        if (zeros % 2 == 0 && ones % 2 == 0) println("YES")
-        else if (pairs.size == 2) {
-            if (pairs[0] == pairs[1] - 1) println("YES") else println("NO")
-        } else if (zeros > ones || ones > zeros || ones == zeros) {
-            for (i in 0 until pairs.size - 1) {
-                if (pairs[i] == pairs[i + 1] - 1) {
-                    --ones
-                    --zeros
+        var even = pairs.filter { it % 2 == 0 }.count() % 2 == 0
+        if (!even) {
+            for (i in pairs.indices) {
+                if (pairs[i] % 2 == 0) {
+                    if ((pairs.contains(pairs[i] - 1) || pairs.contains(pairs[i] + 1)))
+                        even = true
+
                 }
             }
-            if (ones % 2 == 0 && zeros % 2 == 0) println("YES") else println("NO")
-        } else println("NO")
+        }
+        if (even) println("YES") else println("NO")
 
     }
 }
